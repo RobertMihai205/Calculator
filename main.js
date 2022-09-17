839bfe4c01bd71b3dc357f029a0bfad2f1ff359b
Robert205 <robertmihai300@gmail.com>
2022-09-17 14:47:30 +0300

    Rework calculator function

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 //Calculator functions to add, subtract etc.
-function add(a,b){
-    return a + b;
-}
-
-function subtract(a,b){
-    return a-b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
-}
-
+function calculator (a, b, c) {
+    let output = 0;
+  
+    try {
+  
+      switch(c) {
+        case '+':
+          output = a + b
+          break;
+  
+        case '*':
+          output = a * b
+          break;
+  
+        case '-':
+          output = a - b
+          break;
+  
+       case '/': 
+          if (b === 0) {
+            throw "Can't divide by 0!"
+          } else {
+            output = a / b
+          }
+          break;
+      }
+    }
+    catch(e) {
+      console.log("There's an error: ", e)
+    }
+  
+    return output;
+  }
+  
+//Create a new function operate that takes an operator and 2 numbers 
+//and then calls one of the above functions on the numbers.
